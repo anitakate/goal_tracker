86d6629404e04fdfa1570866a41394a8248d5ff4
David Cisneros <d.cisneros849@gmail.com>
2018-09-20 00:17:33 -0400

    added view engine

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-// Load HTTP module
-var http = require("http");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// Create HTTP server and listen on port 8000 for requests
-http.createServer(function(request, response) {
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-   // Set the response HTTP header with HTTP status and Content type
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+var app = express();
 
-   // Send the response body "Hello World"
-   response.end('Hello World\n');
-}).listen(8000);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// Print URL for accessing server
-console.log('Server running at http://127.0.0.1:8000/');  
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
