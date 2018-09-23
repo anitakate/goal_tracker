8b4b568270d803f95336d1be6cabc0bfaf2291bb
David Cisneros <d.cisneros849@gmail.com>
2018-09-23 00:49:17 -0400

    refs #1 Put in a console.log to ensure db is running

--- app.js
+++ app.js
@@ -27,9 +27,15 @@ app.use('/users', usersRouter);
 mongoose.connect(
   "mongodb://localhost:27017/" + process.env.DB_URI,
   {
-    useNewUrlParser: true 
+    useNewUrlParser: true
   }
 );
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("DB running...")
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
